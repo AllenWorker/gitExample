fb56dbb76a5ef366132ec914f05724b6c726c90b
YuiLinNova <allen88038@gmail.com>
2018-09-19 09:32:59 +0800

    Added Employee class

---
+++ src/gitexample/Employee.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitexample;
+
+/**
+ * Holds employee data
+ * @author J392018
+ */
+public class Employee {
+    private String name;
+    private int employeeNum;
+
+    public Employee(String name, int employeeNum) {
+        this.name = name;
+        this.employeeNum = employeeNum;
+    }
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployeeNum() {
+        return employeeNum;
+    }
+
+    public void setEmployeeNum(int employeeNum) {
+        this.employeeNum = employeeNum;
+    }
+    
+    public String PrintDetails()
+    {
+        return employeeNum + " " + name;
+    }
+    
+}
